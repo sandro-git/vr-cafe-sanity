fb36aa5728ceb7cad4962dcbe974816263b669d3
sandro <sandroazerty@icloud.com>
2022-07-27 13:42:55 +0200

    first commit

---
+++ schemas/game.js
@@ -0,0 +1,12 @@
+export default {
+    name:"game",
+    type:"document",
+    title:"Game",
+    fields:[
+        {
+            name:"name",
+            type:"string",
+            title:"Name"
+        }
+    ]
+}
